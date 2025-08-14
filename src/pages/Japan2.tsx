fea95f6785444093a6b4eccd7273201147a853f1
prawnzyy <jamcookies77@gmail.com>
2025-08-14 17:14:28 +0800

    Minor update to wording and image resize

--- src/pages/Japan2.tsx
+++ src/pages/Japan2.tsx
@@ -34,36 +34,37 @@ export default function Japan2() {
             </div>
             <div className="flex flex-col md:px-12 py-4 px-4">
                 <BlogSection title="Day 1: Arriving in Japan">
-                    This was the first day we arrived in Japan en route to Canada. We decided to spend about a week here before starting on our SEP for real. 
-                    The most notable part of day 1 was visiting Shibuya Sky and taking in the sights.
+                    This was our first day in Japan, en route to Canada. We decided to spend about a week here before officially starting our SEP. 
+                    The highlight of the day was visiting Shibuya Sky and enjoying the stunning views.
                     <PictureCarousel forBlog={true} images={[Shibuya1, Shibuya2, Shibuya3]} />
                 </BlogSection>
                 <BlogSection title="Day 2: Imperial Garden and chilling">
-                    We started off the day heading to the Imperial Palace East Gradens for a nice morning walk and exploring the area. In the afternoon, we did some light
-                    shopping in the Ginza area and also stopped by Akihabara for dinner and some more shopping.
+                    We began the day with a relaxing morning walk at the Imperial Palace East Gardens, taking time to explore the area. 
+                    In the afternoon, we did some light shopping in Ginza before heading to Akihabara for dinner and a bit more shopping.
                     <PictureCarousel forBlog={true} images={[Imperial1, Imperial2, Imperial3]} />
                 </BlogSection>
                 <BlogSection title="Day 3: Kamakura and Enoshima">
-                    For this day, we headed to the town of Kamakura to see the giant buddha statue and also explore this quiant little town. After that we headed straight to
-                    the island of Enoshima to enjoy the seaside views and explore the caves. We ended the day in Yokohama for some more light shopping.
+                    We spent the day in the town of Kamakura, visiting the famous Great Buddha statue and exploring the quaint streets. 
+                    From there, we headed straight to Enoshima Island to enjoy the seaside views and explore its caves. 
+                    We ended the day in Yokohama with some light shopping.
                     <PictureCarousel forBlog={true} images={[Enoshima1, Enoshima2, Enoshima3, Enoshima4, Enoshima5]} />
                 </BlogSection>
                 <BlogSection title="Day 4: Actually chilling">
-                    Nothing much took place on this day. Each of us had their own plans and did our own form of exploration. As for me, I just took it chill and walked around.
+                    Nothing much took place on this day. Each of us had their own plans and did our own form of exploration. As for me, I just took it easy and walked around.
                 </BlogSection>
                 <BlogSection title="Day 5: Headed to Irumashi">
-                    As we prepared to do a hike in the Saitama area, we decided to move out of Central Tokyo towards Irumashi. It defintely looked very different from the urban landscape that was Central Tokyo,
-                    but was still nice nonetheless.
+                    As we prepared for a hike in the Saitama area, we moved out of central Tokyo towards Irumashi. 
+                    The scenery was definitely a change from the bustling urban landscape of central Tokyo, but it had its own charm.
                     <PictureCarousel forBlog={true} images={[Irumashi1, Irumashi2, Irumashi3]} />
                 </BlogSection>
                 <BlogSection title="Day 6: Koburi Pass">
-                    The hike we planned to do was the Koburi Pass. Waking up bright and early, we made our way over to the start point and started our hike up. The path that we chose at the start was certainly interesting, leading us to go along some very steep cliffs and dense vegetation.
-                    However, it seemed to be the right path as after an hour of walking, we broke out and found ourselves at a Soba shop where we had lunch. At some point, we realised we could not make it to our designated end point, so we tried to quickly make our way back down. The path we chose
-                    was an interesting one. Reminded me of just bashing through the forest while being led by some red markers on the trees.
+                    Our planned hike for the day was the Koburi Pass. Waking up bright and early, we made our way to the trailhead and began our ascent. 
+                    The path we chose at the start was certainly interesting, taking us along steep cliffs and through dense vegetation. Fortunately, it seemed we were on the right track, because after about an hour we emerged at a soba shop, where we stopped for lunch. 
+                    At some point, we realised we would not make it to our intended end point, so we hurried back down. The route we took was another adventure in itself — it felt like bashing through the forest, guided only by red markers on the trees.
                     <PictureCarousel forBlog={true} images={[Koburi1, Koburi2, Koburi3, Koburi4]} />
                 </BlogSection>
                 <BlogSection title="Day 7 & 8: Winding down and heading to Canada">
-                    Nothing too much happened on the last 2 days. We took things easy after the tiring hike from the day before and just did some minor exploration and last minute shopping before having to head to the airport and onwards to Canada.
+                    Nothing too much happened on the last 2 days. We took things easy after the tiring hike from the day before and did some minor exploration and last minute shopping before heading to the airport and onwards to Canada.
                 </BlogSection>
             </div>
         </div>
